cd475c83e5eec43822f06710df41ff8179eceaae
Orlando <69806200+JuiceDev20@users.noreply.github.com>
2021-02-03 15:52:32 -0500

    Layout changes

--- Models/ViewModels/MyTopTicketsViewModel.cs
+++ Models/ViewModels/MyTopTicketsViewModel.cs
@@ -1,18 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace StatusTracker.Models.ViewModels
 {
     public class MyTopTicketsViewModel
     {
+        public MyTopTicketsViewModel()
+        {
+            Tickets = new List<Ticket>();
+            TicketPriorities = new List<TicketPriority>();
+            TicketStatuses = new List<TicketStatus>();
+            TicketTypes = new List<TicketType>();
+        }
+
         public Ticket Ticket { get; set; }
+
+        public List<Ticket> Tickets { get; set; }
 
         public TicketPriority TicketPriority { get; set; }
 
+        public List<TicketPriority> TicketPriorities { get; set; }
+
         public TicketStatus TicketStatus { get; set; }
+
+        public List<TicketStatus> TicketStatuses { get; set; }
 
         public TicketType TicketType { get; set; }
 
-        public ICollection<Ticket> Tickets { get; set; }
+        public List<TicketType> TicketTypes { get; set; }
+
 
         public Project Project { get; set; }
 
